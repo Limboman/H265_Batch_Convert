441df7f951ee0589403b4a1ce569b1eee70af145
Limboman <limboman31@gmail.com>
2017-12-29 16:33:13 +1100

    added folder checking and file movement

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 from ffmpy import FFmpeg, FFprobe
 import os
 import subprocess
+import shutil
 from pymediainfo import MediaInfo
 
-cwd = os.getcwd() #gets the working directory
+cwd = os.getcwd()  # gets the working directory
 os.environ['PATH'] = os.path.dirname(cwd+'\\Mediainfo.dll') + ';' + os.environ['PATH']
 
-os.chdir('F:\\testing')
+os.chdir('E:\\testing')
 cwd = os.getcwd()
 
 video = False
 
-for file in os.listdir(cwd):
-    fileInfo = MediaInfo.parse(file)
+for files in os.listdir(cwd):
+    fileInfo = MediaInfo.parse(files)
     video = False
     for track in fileInfo.tracks:
         if track.track_type == "Video":
             video = True
 
-    if video == True:
+    if video:
+
         fp = FFprobe(inputs={'test.mp4': '-v error -select_streams v:0 -show_entries stream=codec_name,height,width -of default=noprint_wrappers=1:nokey=1'})
         probe = fp.run(stdout=subprocess.PIPE)
         result = probe[0].split('\r\n')
         print result
         if result[2] >= 700 and result[0] != 'h265':
+            if not os.path.exists(cwd + '\\converted\\'):
+                os.mkdir(cwd + '\\converted\\')
             print 'convert'
-            print file.split('.')[0] + '[HEVC].mp4'
+            print files.split('.')[0] + '[HEVC].mp4'
             ff = FFmpeg(
-                inputs={file: '-hide_banner'},
-                #inputs={file: '-hide_banner -hwaccel cuvid'},
-                outputs={file.split('.')[0] + '[HEVC].mp4': '-c:a aac -c:v libx265 -preset slow'}
-                #outputs={file.split('.')[0] + '[HEVC].mp4': '-c:a aac -c:v hevc_nvenc -preset slow'}
+                inputs={files: '-hide_banner'},
+                # inputs={files: '-hide_banner -hwaccel cuvid'},
+                outputs={files.split('.')[0] + '[HEVC].mp4': '-c:a aac -c:v libx265 -preset slow'}
+                # outputs={files.split('.')[0] + '[HEVC].mp4': '-c:a aac -c:v hevc_nvenc -preset slow'}
             )
             ff.run()
+            shutil.move(files, cwd + '\\converted\\' + files)
         else:
-            #move file
-            shutil.move(file, cwd + '\\converted\\' + file)
+            if not os.path.exists(cwd + '\\to_small_to_convert\\'):
+                os.mkdir(cwd + '\\to_small_to_convert\\')
+            # move file
+            shutil.move(files, cwd + '\\to_small_to_convert\\' + files)
 
 
